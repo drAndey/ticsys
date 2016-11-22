1790abf42b244d906876f44d1fe59a07bfee7877
andy <andreas.reimann.sui@gmail.com>
2016-11-22 16:20:45 +0100

    Andy

--- model/Event.php
+++ model/Event.php
@@ -1,12 +1,11 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: andy
- * Date: 22.11.16
- * Time: 16:14
- */
 class Event
 {
+    private $id;
+    private $name;
+    private $starttime;
+    private $description;
+    private $image;
 
 }
